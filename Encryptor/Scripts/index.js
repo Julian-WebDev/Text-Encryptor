95f0ab9ebaecc5279bed8f0c909035dc7feda6d4
Julian Hidalgo <julian.hidalgo.leon28@gmail.com>
2024-03-05 13:58:14 -0600

    [General][Proyect load]

---
+++ Encryptor/Scripts/index.js
@@ -0,0 +1,25 @@
+
+function encrypt(){
+    let text = document.getElementById('txt_enc').value;
+    let string = text;
+    let mapObj = {
+        e:'enter',
+        a:'ai',
+        i:'imes',
+        o:'ober',
+        u:'ufat'
+    };
+    string = string.replace(/[eaiou]/g, function(matched){
+        return mapObj[matched];
+        
+    });
+    let output = document.getElementById('output');
+    output.innerHTML = string;
+}
+
+function uncrypt(){
+    let unc_text = document.getElementById('txt_unc').value;
+    unc_text=unc_text.replaceAll("enter","e").replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
+    let un_output = document.getElementById('un_output');
+    un_output.innerHTML = unc_text;
+}
